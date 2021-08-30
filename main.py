3867565aa38c098468223f38d42eeddf8e4abb24
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2021-08-30 16:38:21 +0200

    changed layout of the app

--- main.py
+++ main.py
@@ -1,40 +1,79 @@
-
 import time
-from typing import ItemsView
-from item import Item
-from item import inventory
-from item import shop_items1
 
-# your money
-money = 10000
+shop_items=[f"Name: 'Dino', Info: 'A big dino', Price: '{str(10)}'",
+            f"Name: 'Sword', Info: 'A epic sword', Price: '{str(3)}'",
+            f"Name: 'Kiwi', Info: 'Kiwi yum', Price: '{str(50)}'"]
+
+inventory = []
+jobs = ["Job: Programmer, Income: 100", "Job: YouTuber, Income: 200", "Job: Artist, Income: 300"]
+
+money = 0
+
+def buy_item(Name):
+    for item in shop_items:
+        item=item.split("'")
+        if item[1] == Name:
+            global money
+            if money >= int(item[5]):
+                inventory.append(item)
+                money = money - int(item[5])
+            else:
+                print("You don't have enough money!")
+                time.sleep(1)
+                print("Redirecting you back....")
+                time.sleep(1)
+                start()
+
+def work(job):
+    if job == "Programmer":
+        income = 100
+        global money
+        money = money + income
+        print(f"You worked as a {job} and got {income}!")
+    elif job == "YouTuber":
+        income = 200
+        money = money + income
+        print(f"You worked as a {job} and got {income}!")
+    elif job == "Artist":
+        income = 300
+        money = money + income
+        print(f"You worked as a {job} and got {income}!")
+    else:
+        print("Invalid job name!")
+
+def job_prompt():
+    print(jobs)
+    print("----------------------------------------------------")
+    prompt = input("What job do you want to work as?: ")
+    work(prompt)
+    time.sleep(2)
+    print(f"Your balance is now: {money}€!")
+    start()
 
 def open_shop():
+    print("----------------------------------------------------")
+    print(shop_items)
+    print("----------------------------------------------------")
+    item = input("What do you want to buy?: ")
+    buy_item(item)
+    time.sleep(2)
+    print(f"Successfully bought {item}!")
+    time.sleep(1)
+    print(f"Your balance is now {money}€!")
+    time.sleep(1)
+    print("Redirecting you back....")
+    start()
 
-    Item.buy_item()
 
-def do_shit():
-    
-    # create an item here => Name of the item, description, and price as int
-    Item(name="Pizza", desc="Delicious pizza, yum!", price=25)
-    
-    print("----------------------")
-    question = input("Do you wanna open your inventory? y/n: ")
 
-    if question == "y":
-        print("----------------------")
-        print(inventory)
-        print("----------------------")
-        do_shit()
+def start():
+    print("----------------------------------------------------")
+    prompt1 = input("What do you want to do?\nA: Buy something, B: Work and get money: ")
 
-    elif question == "n":
+    if prompt1 == "A":
+        open_shop()
 
-        shop_question = input("Do you wanna open the shop? y/n: ")
-        print("----------------------")
+    elif prompt1 == "B":
+        job_prompt()
 
-        if shop_question == "y":
-            open_shop()
-        elif shop_question == "n":
-            do_shit()
-
-do_shit()
-
+start()
