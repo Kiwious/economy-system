49b1f3baad4e3b843a0966b9efa7e366d2523b18
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2021-08-30 11:43:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ def add_item():
     i = input("What item do you wanna create?: ")
     time.sleep(1)
     desc = input("What description?: ")
+    time.sleep(1)
+    price = input("What price?: ")
     
     # creating the item and putting it into the inventory
-    created_item = Item(i, desc)
+    created_item = Item(name=i, desc=desc, price=price)
     inventory.append(created_item.name)
 
-    q = input("Do you wanna open your inventory? Y/N: ")
+    q = input("Do you wanna open your inventory? y/n: ")
 
     if q == "y":
         print(inventory)
