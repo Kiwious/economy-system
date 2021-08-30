6b130b1faf1849cff0f5577bf8d7e4a78855cda3
kiwiouskiwi <84627732+Kiwious@users.noreply.github.com>
2021-08-30 12:44:48 +0200

    added shop

--- item.py
+++ item.py
@@ -1,4 +1,8 @@
+
+
 inventory = []
+shop_items = []
+
 
 class Item:
 
@@ -7,4 +11,19 @@ class Item:
         self.desc = desc
         self.price = price
 
-    
+        shop_items.append(f"Item: {self.name}, Description: {self.desc}, Price: {self.price}")
+
+    @classmethod
+    def buy_item(cls):
+        from main import money
+        question = input(f"What product do you wanna buy? {str(shop_items)}\nYour available Money: {money}\n\n")
+
+        if question in shop_items:
+            
+            if cls.price >= money:
+                inventory.append(f"{cls.name}, {cls.desc}")
+                print(f"{cls.name} successfully bought!")
+            else: 
+                print("Item not found!")
+
+
--- main.py
+++ main.py
@@ -1,24 +1,34 @@
-from item import *
+
 import time
+from item import Item
+from item import inventory
+from item import shop_items
+
+money = 10000
+
 
 
 def add_item():
-    i = input("What item do you wanna create?: ")
-    time.sleep(1)
-    desc = input("What description?: ")
-    time.sleep(1)
-    price = input("What price?: ")
     
-    # creating the item and putting it into the inventory
-    created_item = Item(name=i, desc=desc, price=price)
-    inventory.append(created_item.name)
+    # create an item here => Name of the item, description, and price as int
+    Item(name="Kiwi", desc="Kiwis are the best", price=100)
+    Item(name="Discord", desc="The whole discord company.", price=6969696969)
 
     q = input("Do you wanna open your inventory? y/n: ")
 
+
     if q == "y":
         print(inventory)
+        time.sleep(5)
+        print("----------------------")
+        add_item()
+
+    elif q == "n":
+        Item.buy_item()
+
     else:
         print("----------------------")
         add_item()
 
 add_item()
+
